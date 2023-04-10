93b70959309c19855eb13ba11ea85b51aff2956c
Gevorg Hovhannisyan <gehovhan@c1r4s4.42yerevan.am>
2023-04-10 15:09:50 +0400

    libft

---
+++ ft_strchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gehovhan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 17:45:22 by gehovhan          #+#    #+#             */
+/*   Updated: 2023/01/19 17:45:25 by gehovhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	char	*ptr;
+
+	ptr = (char *)str;
+	while (*ptr)
+	{
+		if (*ptr == (unsigned char)c)
+			return (ptr);
+		ptr++;
+	}
+	if (c == 0)
+		return (ptr);
+	return (NULL);
+}
